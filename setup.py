19dc6ea5f8de33b4eb9b0e890002dd7727dc8cab
mattvlaw <mattvlaw@gmail.com>
2018-10-12 21:01:39 -0400

    added package indicators, message, srv, action generators, action files, etc. to CMakeLists and package.xml, along with a setup.py, which had to be referenced in cmakelists

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DaBot',
+    version='0.0.0',
+    packages=['daarm', 'daagent', 'datui', 'dautils'],
+    package_dir={'': 'src'},
+    license='MIT license',
+)
