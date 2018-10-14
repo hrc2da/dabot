4599077491221f5496630ba00e5459dc8e5ba278
mattvlaw <mattvlaw@gmail.com>
2018-10-13 21:24:54 -0400

    added some services to daarm

--- src/daarm/daarm_server.py
+++ src/daarm/daarm_server.py
@@ -17,10 +17,12 @@ import rospy
 import actionlib
 from dabot.msg import CalibrateAction, CalibrateFeedback, CalibrateResult, MoveBlockAction, MoveBlockFeedback, MoveBlockResult
 from dabot.msg import CalibrationParams
-from dabot.srv import TuiState
-from dautils import get_arm_bounds, get_tuio_bounds, tuio_to_ratio
+from dabot.srv import TuiState, TuiStateRequest
+from dautils import get_ros_param, get_arm_bounds, get_tuio_bounds, tuio_to_ratio
 from geometry_msgs.msg import Point, Quaternion
 from std_msgs.msg import String
+from kinova_msgs.msg import *
+from kinova_msgs.srv import *
 from moveit_commander import RobotCommander, MoveGroupCommander
 import json
 
@@ -29,22 +31,30 @@ class DaArmServer:
     """The basic, design problem/tui agnostic arm server
     """
     gestures = {}
+    grasp_height = 0.1
 
     def __init__(self, num_planning_attempts=10):
         rospy.init_node("daarm_server", anonymous=True)
         self.calibration_publisher = rospy.Publisher("/calibration_results", CalibrationParams)
         rospy.wait_for_service('get_tui_blocks')
         self.get_block_state = rospy.ServiceProxy('get_tui_blocks', TuiState)
+        self.init_params()
         self.init_publishers()
         self.init_arm()
 
-    def init_arm(self, num_planning_attempts=10):
+    def init_arm(self, num_planning_attempts=20):
         self.arm = MoveGroupCommander("arm")
         self.gripper = MoveGroupCommander("gripper")
         self.robot = RobotCommander()
         self.arm.set_num_planning_attempts(num_planning_attempts)
         self.arm.set_goal_tolerance(0.2)
         self.init_action_servers()
+
+    def init_params(self):
+        try:
+            self.grasp_height = get_ros_param("GRASP_HEIGHT", "Grasp height defaulting to 0.1")
+        except ValueError as e:
+            rospy.loginfo(e)
 
     def init_publishers(self):
         self.action_belief_publisher = rospy.Publisher("/arm_action_beliefs", String, queue_size=1)
@@ -53,6 +63,39 @@ class DaArmServer:
     def init_action_servers(self):
         self.calibration_server = actionlib.SimpleActionServer("calibrate_arm", CalibrateAction, self.calibrate)
         self.move_block_server = actionlib.SimpleActionServer("move_block", MoveBlockAction, self.handle_move_block)
+
+    def init_action_clients(self):
+        # Action Client for joint control
+        action_address = '/j2s7s300_driver/joints_action/joint_angles'
+        self.jointActionClient = actionlib.SimpleActionClient(action_address, kinova_msgs.msg.ArmJointAnglesAction)
+        rospy.loginfo('Waiting for ArmJointAnglesAction server...')
+        self.jointActionClient.wait_for_server()
+        rospy.loginfo('ArmJointAnglesAction Server Connected')
+
+    def init_services(self):
+        # Service to get TUI State
+        self.tui_state_service = rospy.ServiceProxy('/get_tui_blocks', TuiState)
+
+        # Service for homing the arm
+        home_arm_service = '/j2s7s300_driver/in/home_arm'
+        self.home_arm_service = rospy.ServiceProxy(home_arm_service, HomeArm)
+        rospy.loginfo('Waiting for kinova home arm service')
+        rospy.wait_for_service(home_arm_service)
+        rospy.loginfo('Kinova home arm service server connected')
+
+        # Service for emergency stop
+        emergency_service = '/j2s7s300_driver/in/stop'
+        self.emergency_stop = rospy.ServiceProxy(emergency_service, Stop)
+        rospy.loginfo('Waiting for Stop service')
+        rospy.wait_for_service(emergency_service)
+        rospy.loginfo('Stop service server connected')
+
+        # Service for restarting the arm
+        start_service = '/j2s7s300_driver/in/start'
+        self.restart_arm = rospy.ServiceProxy(start_service, Start)
+        rospy.loginfo('Waiting for Start service')
+        rospy.wait_for_service(start_service)
+        rospy.loginfo('Start service server connected')
 
     def home_arm(self):
         # send the arm home
@@ -98,27 +141,54 @@ class DaArmServer:
         plan = self.arm.plan()
         if plan:
             if action_server:
-                action_server.publish_feedback(CalibrateFeedback("Plan Succeeded"))
+                action_server.publish_feedback(CalibrateFeedback("Plan Found"))
             self.arm.execute(plan)
             rospy.sleep(delay)
         else:
-            action_server.publish_feedback(CalibrateFeedback("Planning Failed"))
+            if action_server:
+                action_server.publish_feedback(CalibrateFeedback("Planning Failed"))
 
     def handle_move_sequence(self, message):
         pass
 
-    def pick_block(self):
+    def get_grasp_orientation(self, position):
+        return Quaternion(1, 0, 0, 0)
+
+    def move_z(self, distance):
+        p = self.arm.get_current_pose()
+        p.pose.position.z += distance
+        self.move_arm_to_pose(p.pose.position, p.pose.orientation, delay=0)
+
+    def pick_block(self, location, check_grasp=False, retry_attempts=0, delay=0, action_server=None):
         # go to a spot and pick up a block
-        pass
+        # if check_grasp is true, it will check torque on gripper and retry or fail if not holding
+        p = self.arm.get_current_pose()
+        position = Point(location.x, location.y, self.grasp_height)
+        orientation = self.get_grasp_orientation(position)
+        try:
+            self.move_arm_to_pose(position, orientation, delay=0, action_server=action_server)
+        except Exception as e:
+            raise(e)
+        if action_server:
+            action_server.publish_feedback()
 
-    def place_block(self):
+        if check_grasp is True:
+            for i in range(retry_attempts):
+                self.move_z(0.3)
+
+    def place_block(self, location, check_grasp=False):
         # go to a spot an drop a block
+        # if check_grasp is true, it will check torque on gripper and fail if not holding a block
+
         pass
 
-    def stop_motion(self):
+    def stop_motion(self, home=False):
         # cancel the moveit_trajectory
         self.arm.stop()
-        # do I need to do anything to kinova?
+        # call the emergency stop on kinova
+        self.emergency_stop()
+        if home is True:
+            self.home_arm()
 
     def calibrate(self, message):
         print("calibrating ", message)
